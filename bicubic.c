93e0ec1a0e48396b3f3ed9ba929c1f6a056f4bca
David Zaslavsky <diazona@ellipsix.net>
2012-10-15 00:46:10 -0400

    Add GPL notice at top of file

--- bicubic.c
+++ bicubic.c
@@ -1,3 +1,25 @@
+/*
+ * This file is part of interp2d, a GSL-compatible two-dimensional
+ * interpolation library. <http://www.ellipsix.net/interp2d.html>
+ *
+ * Copyright 2012 David Zaslavsky
+ * Portions based on GNU GSL interpolation code,
+ *  copyright 1996, 1997, 1998, 1999, 2000, 2004 Gerard Jungman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_interp.h>
 #include <math.h>
